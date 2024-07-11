ef6e7d59ad44781a2a67d5eb4a517429bf123888
DucTrung611 <thanductrung123@gmail.com>
2024-07-11 11:00:50 +0700

    first commit

---
+++ 00-react-javascript/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import axios from './util/axios.customize';
+
+function App() {
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      const res = await axios.get(`/v1/api`);
+      console.log("check res", res);
+    }
+    fetchHelloWorld();
+  }, [])
+  return (
+    <>
+      hello world
+
+    </>
+  )
+}
+
+export default App
